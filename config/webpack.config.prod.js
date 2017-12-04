f82fb6a39a37e785d94aaca688c0925b8ece65be
Lix <lix@shinemo.com>
2017-12-04 18:39:45 +0800

    add preset-minify / bundle-analyzer

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -12,6 +12,7 @@ const eslintFormatter = require('react-dev-utils/eslintFormatter');
 const ModuleScopePlugin = require('react-dev-utils/ModuleScopePlugin');
 const paths = require('./paths');
 const getClientEnvironment = require('./env');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 // Webpack uses `publicPath` to determine where the app is being served from.
 // It requires a trailing slash, or the file assets will get an incorrect path.
@@ -234,6 +235,8 @@ module.exports = {
     ],
   },
   plugins: [
+    // add webpack-bundle-analyzer
+    new BundleAnalyzerPlugin(),
     // Makes some environment variables available in index.html.
     // The public URL is available as %PUBLIC_URL% in index.html, e.g.:
     // <link rel="shortcut icon" href="%PUBLIC_URL%/favicon.ico">
